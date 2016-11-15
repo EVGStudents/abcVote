ac435fd0742c410c77721d5902d92a3e90aee023
Sebastian Nellen <sebastian@nellen.it>
2016-11-15 13:44:58 +0100

    mysql default port not needed in configuration.php, causing problems

--- bulletinBoard/src/public/configuration.php
+++ bulletinBoard/src/public/configuration.php
@@ -2,7 +2,7 @@
   $config['displayErrorDetails'] = true;
   $config['addContentLengthHeader'] = false;
 
-  $config['db']['host']   = "127.0.0.1:3306";
+  $config['db']['host']   = "127.0.0.1";
   $config['db']['user']   = "abcVoter";
   $config['db']['pass']   = "RpRjwLVvATGKJsfR";
   $config['db']['dbname'] = "abcVote";
