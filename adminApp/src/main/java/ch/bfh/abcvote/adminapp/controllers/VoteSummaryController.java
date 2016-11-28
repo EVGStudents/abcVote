78a36113126224a50978f14d696ca6886d4c1f95
Timo Buerk <burkt4@gmail.com>
2016-11-28 19:38:29 +0100

    adminApp + voterApp: fleshed out summary screens

--- adminApp/src/main/java/ch/bfh/abcvote/adminapp/controllers/VoteSummaryController.java
+++ adminApp/src/main/java/ch/bfh/abcvote/adminapp/controllers/VoteSummaryController.java
@@ -8,13 +8,20 @@ package ch.bfh.abcvote.adminapp.controllers;
 import ch.bfh.abcvote.adminapp.AdminApp;
 import ch.bfh.abcvote.adminapp.ControlledScreen;
 import ch.bfh.abcvote.util.model.Vote;
+import ch.bfh.abcvote.util.model.VoteTopic;
+import ch.bfh.abcvote.util.model.Voter;
 import ch.bfh.unicrypt.UniCryptException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 /**
  * FXML Controller class
@@ -30,6 +37,18 @@ public class VoteSummaryController implements Initializable, ControlledScreen {
     private Button btBack;
     @FXML
     private Button btCreate;
+    @FXML
+    private ListView<Voter> lvVoters;
+    @FXML
+    private ListView<String> lvOptions;
+    @FXML
+    private Label lbTitle;
+    @FXML
+    private Label lbTopic;
+    @FXML
+    private Label lbPick;
+    @FXML
+    private Label lbVotingPeriod;
 
     /**
      * Initializes the controller class.
@@ -66,6 +85,32 @@ public class VoteSummaryController implements Initializable, ControlledScreen {
     @Override
     public void setScene(Vote vote) {
         this.vote = vote;
+        displayVote(vote);
+    }
+
+    private void displayVote(Vote vote) {
+        VoteTopic topic = vote.getTopic();
+        String votingPeriodString = vote.getStartDate().toString() + " - " + vote.getEndDate().toString();
+        lbTitle.setText(vote.getTitle());
+        lbVotingPeriod.setText(votingPeriodString);
+        populateVotersListView(vote.getVoterList());
+        lbTopic.setText(topic.getTitle());
+        lbPick.setText(String.valueOf(topic.getPick()));
+        populateOptionsListView(topic.getOptions());
+    }
+    
+    private void populateOptionsListView(List<String> optionsList) {
+        
+        ObservableList<String> listViewList = FXCollections.observableArrayList(optionsList);
+        lvOptions.setItems(listViewList);
+        
+    }
+    
+    private void populateVotersListView(List<Voter> votersList) {
+        
+        ObservableList<Voter> listViewList = FXCollections.observableArrayList(votersList);
+        lvVoters.setItems(listViewList);
+        
     }
     
 }
