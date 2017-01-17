7c74942f4343a4faa60a4b8b9df32e0843a3e2cb
Sebastian Nellen <sebastian@nellen.it>
2017-01-17 17:31:53 +0100

    added license header for voterApp package

--- voterApp/src/main/java/ch/bfh/abcvote/voterapp/ControlledScreen.java
+++ voterApp/src/main/java/ch/bfh/abcvote/voterapp/ControlledScreen.java
@@ -1,7 +1,39 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * abcVote
+ *
+ *  abcVote - an e-voting prototype with everlasting privacy
+ *  Copyright (c) 2017 Timo Buerk and Sebastian Nellen
+ *
+ *  Licensed under Dual License consisting of:
+ *  1. GNU Affero General Public License (AGPL) v3
+ *  and
+ *  2. Commercial license
+ *
+ *
+ *  1. This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *  2. Licensees holding valid commercial licenses for abcVote may use this file in
+ *   accordance with the commercial license agreement provided with the
+ *   Software or, alternatively, in accordance with the terms contained in
+ *   a written agreement between you and us.
+ *
+ *
+ *   For further information contact <e-mail: burkt4@gmail.com> or <e-mail: sebastian@nellen.it>
+ *
+ *
+ * Redistributions of files must retain the above copyright notice.
  */
 package ch.bfh.abcvote.voterapp;
 
